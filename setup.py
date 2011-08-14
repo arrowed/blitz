5338ec720b538021b94775c87ba7dd21d957575f
arrowed <manny@.(none)>
2011-08-14 12:07:11 +1000

    Switched version to 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blitz',
-    version=0.1.0,
+    version='0.1.0',
     description='Blitz is a web based performance stress testing tool',
     long_description="",
     author='Guilherme Hermeto',
