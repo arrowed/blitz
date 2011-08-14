82a5133e8c2eb6e4f027237211a40b610695db63
arrowed <manny@.(none)>
2011-08-14 12:04:43 +1000

    Added very basic setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name='blitz',
+    version=get_version('short'),
+    description='Blitz is a web based performance stress testing tool',
+    long_description="",
+    author='Guilherme Hermeto',
+    author_email='gui hermeto at gmail com',
+    url='http://blitz.io',
+    packages=find_packages(),
+    test_suite='unittest',
+    tests_require=['nose', 'fudge'],
+    install_requires=,
+    entry_points={
+        
+    },
+    classifiers=[
+          
+    ],
+)
